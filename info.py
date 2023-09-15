9c31c87c9a752684960fba9edee76305c0e347b1
Rokstar890 <144723163+Rokstar890@users.noreply.github.com>
2023-09-15 08:18:06 -0700

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,7 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = 
-ADMIN        = 
-CHANNEL      = "@CyNiTeBaCkUp"
+API_ID       = 23762978
+API_HASH     = "eff30dac5504a8660e69bfe19f668571"
+BOT_TOKEN    = "6374215608:AAE52zVieLWDPMoaRodyXa4USiEB7NpzlSk"
+SESSION      = "BQBa4UPS8EcRqF_0RLWvvgcRFnkG2iiBNjmXpaJi3kec-BQ-Wbjpbzb3KT7KLOMrUMJE2tvjp1nZkGxinlbve0CczkCdKAEjD3WkhqS2TcvePhyiV8wtQKoM_npc9HvJMj0jGhO0eTRkGPNrR-uVVFKjR4N7cxUKLziaQ0Fj91jNFafdQLd3NDF7jiKscKHUqV0QOr0Q095jmuHrKI7NgNqA2Wvk40OQAueMeV7V2_8AEg7NjYydlXOelTSAe-C1EOzqRMozQcxzIA3F5VIEjrZCR9hIPooUOlOMtK3vithc-lpRRO838bf9-dgCAwl6FXlxdgpJuY7MltkHq-jT4_m-SH9iIAA"
+DATABASE_URI = "mongodb+srv://hebapir376:ravi1234@cluster0.x9pmpfx.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001940288450
+ADMIN        =1216307744
